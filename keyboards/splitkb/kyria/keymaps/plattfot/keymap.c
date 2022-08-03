cf2109f5dc031fa2fb31d804ce584d1860fc1fcd
Steffen Breinbjerg <steffenpb@outlook.dk>
2022-08-04 00:07:10 +0200

    Changes to keymap.

--- keyboards/splitkb/kyria/keymaps/plattfot/keymap.c
+++ keyboards/splitkb/kyria/keymaps/plattfot/keymap.c
@@ -55,28 +55,29 @@ enum layers {
 #define EU_CDOT RALT(KC_EQUAL)
 #define EU_DEG  RALT(KC_SCLN)
 #define RSHIFT_ENT  MT(MOD_RSFT, KC_ENT)
+#define ALT_SPACE  MT(MOD_LALT, KC_SPACE)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: Default
  *
- * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  Esc   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  | BSpace |
- * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |  Tab   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  | Lead | RAISE|  | LOWER|  DEL |   N  |   M  |   ,  |   .  |   /  | RShift |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        | MPlay| GUI  | LCtrl| Space| LALT |  | RCtrl| Enter| NAV  | RALT |Scroll|
- *                        |      |      |      |      |      |  |      |      |      |      | Lock |
+ * ,-------------------------------------------.                              ,-----------------------------------------------'
+ * |  Esc   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  RCtrl     |
+ * |--------+------+------+------+------+------|                              |------+------+------+------+------+------------|
+ * |  Tab   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  |   ;  |   '        |
+ * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+------------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  | Lead | DEL  |  | RALT |      |   N  |   M  |   ,  |   .  |   /  | RShift/ent |
+ * `----------------------+------+------+------+------+------|  |------+------+------+------+------+--------------------------'
+ *                        | MPlay| GUI  | LCtrl| LALT |RAISE |  | Enter|BSpace| NAV  |LOWER |Scroll|
+ *                        |      |      |      | space|      |  |      |      |      |      | Lock |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_DEFAULT] = LAYOUT(                                                                                                                
-      KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    [_DEFAULT] = LAYOUT(
+      KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RCTL,
       KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    KC_LEAD,  L_RAISE, L_LOWER, KC_DEL, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSHIFT_ENT,
-                               KC_MPLY,KC_LGUI,KC_LCTL, KC_SPACE, KC_LALT, KC_RCTL,  KC_ENT, L_NAV,   KC_RALT, KC_SLCK
+      KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    KC_LEAD,  KC_DEL, KC_RALT, KC_DEL, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSHIFT_ENT,
+                               KC_MPLY,KC_LGUI,KC_LCTL, ALT_SPACE, L_RAISE, KC_ENT,  KC_BSPC, L_NAV,   L_LOWER, KC_SLCK
     ),
 
 /*
